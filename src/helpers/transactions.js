22218a92240a641bd9711f5416ba5292fae0e0d9
hanzel98 <hanzel.anchia.mena@cr.ey.com>
2021-11-28 11:26:27 -0600

    feat: added function to process transactions

---
+++ src/helpers/transactions.js
@@ -0,0 +1,52 @@
+/**
+ * This function returns an array with the data of the latest 10 blocks
+ * @param {Object} web3 - web3 instance
+*/
+export const getLatestBlocks = async (web3) => {
+    const latestBlockNumber = await web3.eth.getBlockNumber();
+    // Requesting information for the 10 blocks sequentially
+    const latestBlocksPromises = [];
+    for (let i=0; i < 10; i++) {
+        const blockNumber = latestBlockNumber - i;
+        const blockDataPromise = web3.eth.getBlock(blockNumber);
+        // const blockDataPromise = web3.eth.getBlock(blockNumber).then(data => ({...data, blockNumber})); // This might not be necessary
+        latestBlocksPromises.push(blockDataPromise);
+    }
+    // Resolving block promises
+    const latestBlocks = await Promise.all(latestBlocksPromises);
+    return latestBlocks;
+};
+
+/**
+ * This function returns an array with the transactions data of a given block number,
+ * filtered by the transactions sending ETH
+ * @param {Object} web3 - web3 instance
+ * @param {Array} transactionHashes - An array with the transaction hashes of the block 
+*/
+export const getBlockTransactionsData = async (web3 ,transactionHashes) => {
+    // Requesting information for all transactions sequentially
+    const transactionsPromises = [];
+    for (const transactionHash of transactionHashes) {
+        let transactionPromise = web3.eth.getTransaction(transactionHash);
+        transactionsPromises.push(transactionPromise);
+    }
+    // Resolving transaction promises
+    const allTransactions = await Promise.all(transactionsPromises);
+    const filteredTransactions = allTransactions.filter(tx => tx.value !== '0');
+    console.log('filteredTransactions:', filteredTransactions)
+    return filteredTransactions;
+};
+
+/**
+ * This function returns an array with the filtered transactions where 
+ * the user addres is participating in the "from" or "to" address
+ * @param {Array} blockTransactions - An array with the transaction hashes of the block 
+ * @param {string} userAddress - An array with the transaction hashes of the block 
+*/
+export const filterTransactionsByAddress = (blockTransactions, userAddress) => {
+    return blockTransactions.filter(transaction => {
+        const isUserAddressInFrom = transaction.from === userAddress;
+        const isUserAddressInTo = transaction.to === userAddress;
+        return isUserAddressInFrom || isUserAddressInTo;
+    });
+};
